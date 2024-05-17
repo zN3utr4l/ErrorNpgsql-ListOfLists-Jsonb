a55de6a1a280f5ab31189408eccf57e617d8b10c
Giuseppe Chirico <g.chirico@costal.it>
2024-05-17 23:06:13 +0200

    Console App

--- ErrorNpgsql/Program.cs
+++ ErrorNpgsql/Program.cs
@@ -1,5 +1,6 @@
 using ErrorNpgsql.Configuration;
 using ErrorNpgsql.Datas;
+using ErrorNpgsql.Models;
 using Microsoft.AspNetCore.Mvc;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,19 @@ WebApplication app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/error-jsonb", ([FromServices] CustomDbContext context) => context.MyEntity.ToList()).WithName("ErroJsonb");
+app.MapGet("/error-jsonb", ([FromServices] CustomDbContext context) =>
+{
+    context.Add(new MyEntity()
+    {
+        JsonbFields = [
+            [new() { Key = "key1", Value = "value1"}, new() { Key = "key2", Value = "value2"}],
+            [new() { Key = "key1", Value = "value1"}, new() { Key = "key2", Value = "value2"}]
+        ]
+    });
+
+    context.SaveChanges();
+
+    return context.MyEntity.ToList();
+}).WithName("ErroJsonb");
 
 app.Run();
---
+++ ErrorNpgsqlJsonb/Program.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace ErrorNpgsqlJsonb;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ServiceCollection services = new();
+
+        services.AddDbContext<MyDbContext>(x =>
+        {
+            x.EnableSensitiveDataLogging();
+            x.UseLazyLoadingProxies();
+            x.EnableDetailedErrors();
+            x.UseSnakeCaseNamingConvention();
+            x.UseNpgsql("Host=127.0.0.1:5432;Database=test;Username=postgres;Password=Test123!;Include Error Detail=true;Maximum Pool Size=500;Pooling=true;");
+        });
+
+        ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+        using MyDbContext? dbContext = serviceProvider.GetService<MyDbContext>();
+
+        for (int i = 0; i < 2; i++)
+        {
+            dbContext.Add(new MyEntity()
+            {
+                JsonbFields = [
+                    [new() { Key = "key1", Value = "value1"}, new() { Key = "key2", Value = "value2"}],
+                    [new() { Key = "key1", Value = "value1"}, new() { Key = "key2", Value = "value2"}]
+                ]
+            });
+
+            dbContext.SaveChanges();
+
+            foreach (MyEntity entity in dbContext.MyEntity)
+            {
+                Console.WriteLine(entity.Id);
+
+                foreach (List<JsonbField> innerList in entity.JsonbFields)
+                {
+                    //Console.WriteLine(innerList); // Castle.Proxies.List`1Proxy ???
+
+                    foreach (JsonbField field in innerList) // System.Collections.Generic.List`1[ErrorNpgsqlJsonb.JsonbField] Ok!!
+                    {
+                        Console.WriteLine($"Key: {field.Key}, Value: {field.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
+
+#region entities
+
+public class MyEntity
+{
+    public int Id { get; set; }
+
+    public List<List<JsonbField>>? JsonbFields { get; set; }
+}
+
+public class JsonbField
+{
+    public required string Key { get; set; }
+
+    public required string Value { get; set; }
+}
+
+#endregion
+
+#region dbcontext
+
+public class MyDbContext(DbContextOptions<MyDbContext> options) : DbContext(options)
+{
+    public DbSet<MyEntity> MyEntity { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("public");
+
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<MyEntity>(x => x.OwnsMany(x => x.JsonbFields, r => { r.ToJson(); }));
+
+        Regex camelCaseRegex = new(@"([a-z0-9])([A-Z])", RegexOptions.None, TimeSpan.FromSeconds(5));
+
+        foreach (IMutableEntityType entity in modelBuilder.Model.GetEntityTypes())
+        {
+            entity.SetTableName(camelCaseRegex.Replace(entity.GetTableName(), "$1_$2").ToLower(CultureInfo.InvariantCulture));
+        }
+    }
+}
+
+#endregion
